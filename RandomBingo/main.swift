2b9e4e43a5ac19d69cee61f83fe1432906a51b96
haram22 <kimhr9889s@gamil.com>
2023-04-20 13:52:00 +0900

    random bingo game terminal

--- RandomBingo/main.swift
+++ RandomBingo/main.swift
@@ -7,5 +7,26 @@
 
 import Foundation
 
-print("Hello, World!")
+var computerChoice = Int.random(in: 1...100)
+var myChoice: Int = 0
 
+while true{
+    // 나중에 숫자로 타입변형 필요 (readLine 타입은 optional)
+    var userInput = readLine()
+
+    // string to Int
+    if let input = userInput{
+        if let number = Int(input){
+            myChoice = number
+        }
+    }
+//    print("my choice : " , myChoice)
+    if computerChoice > myChoice {
+        print("💻 computer : Up ⬆️")
+    } else if computerChoice < myChoice {
+        print("💻 computer : Down ⬇️")
+    } else {
+        print("👏🏻 Bingo 👏🏻")
+        break
+    }
+}
